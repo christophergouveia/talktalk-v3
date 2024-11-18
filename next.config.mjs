bd357c04ee84149393c13b79b2cd2a2b1eec7e35
Christopher <34822892+christophergouveia@users.noreply.github.com>
2024-11-17 22:44:12 -0300

    Removendo migrations do git

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin("./app/i18n/request.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "standalone",
   images: {
     remotePatterns: [
       {
